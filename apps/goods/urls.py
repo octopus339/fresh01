acef69960ee38346b388f253366ef0cc746ea0b0
leslie <963082115@qq.com>
2018-05-02 19:03:04 +0800

    创建静态文件夹static 初步显示首页

--- apps/goods/urls.py
+++ apps/goods/urls.py
@@ -1,5 +1,9 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+
+from apps.goods import views
+
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^index$',views.IndexView.as_view(),name='index')
 ]
